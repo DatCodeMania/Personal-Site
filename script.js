21f3f0494444d480eb1e4a64fb9a22a8fed5d87e
DatCodeMania <datcodemania@users.noreply.github.com>
2024-09-29 10:05:33 +0700

    Fix some coloring, add light theme and transition from dark theme

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
     const phrases = [
-        "Hello", "Hola", "Bonjour", "Hallo", "Ciao", "Olá", "Привет", 
-        "こんにちは", "안녕하세요", "你好", "Hej", "Merhaba", "Salam", 
-        "Shalom", "नमस्ते", "สวัสดี", "xin chào", "გამარჯობა", "Sveiki", 
-        "Ahoj", "Hei", "Zdravo", "Прывітанне", "Selam", "Tere", 
-        "Cześć", "Szia", "Γειά σου", "Bună", "नमस्कार", "Sawubona", 
-        "Kamusta", "Jambo", "Salam", "שלום", "Marhaba", "สวัสดี", 
-        "안녕하세요", "今日は", "你好", "God dag", "Hei", "Hallo", 
-        "Bok", "Labdien", "Labas", "Zdravstiky", "Aloha", "Sveiki", 
+        "Hello", "Hola", "Bonjour", "Hallo", "Ciao", "Olá", "Привет",
+        "こんにちは", "안녕하세요", "你好", "Hej", "Merhaba", "Salam",
+        "Shalom", "नमस्ते", "สวัสดี", "xin chào", "გამარჯობა", "Sveiki",
+        "Ahoj", "Hei", "Zdravo", "Прывітанне", "Selam", "Tere",
+        "Cześć", "Szia", "Γειά σου", "Bună", "नमस्कार", "Sawubona",
+        "Kamusta", "Jambo", "Salam", "שלום", "Marhaba", "สวัสดี",
+        "안녕하세요", "今日は", "你好", "God dag", "Hei", "Hallo",
+        "Bok", "Labdien", "Labas", "Zdravstiky", "Aloha", "Sveiki",
         "Hallo", "Hei"
     ];
+
     const typewriter = document.getElementById('typewriter');
+    const themeToggleIcon = document.querySelector('.theme-toggle-icon');
+    let isLightTheme = false;
+
+    themeToggleIcon.addEventListener('click', function() {
+        if (isLightTheme) {
+            document.body.style.backgroundColor = '#2e3440';
+            document.querySelectorAll('.navbar-link').forEach(link => link.style.color = '#eceff4');
+            document.querySelectorAll('.heading, .subheading, .typewriter').forEach(text => text.style.color = '#eceff4');
+            document.querySelector('.navbar-container').style.backgroundColor = 'rgba(216, 222, 233, 0.096)';
+            themeToggleIcon.style.backgroundColor = 'rgba(216, 222, 233, 0.096)';
+            themeToggleIcon.classList.remove('fa-moon');
+            themeToggleIcon.classList.add('fa-sun');
+            themeToggleIcon.style.color = '#eceff4';
+        } else {
+            document.body.style.backgroundColor = '#eceff4';
+            document.querySelectorAll('.navbar-link').forEach(link => link.style.color = '#2e3440');
+            document.querySelectorAll('.heading, .subheading, .typewriter').forEach(text => text.style.color = '#2e3440');
+            document.querySelector('.navbar-container').style.backgroundColor = '#d8dee9';
+            themeToggleIcon.style.backgroundColor = '#d8dee9';
+            themeToggleIcon.classList.remove('fa-sun');
+            themeToggleIcon.classList.add('fa-moon');
+            themeToggleIcon.style.color = '#2e3440';
+        }
+
+        isLightTheme = !isLightTheme;
+    });
+
     let phrase = 'Hello';
     let typing = true; // State to control typing or untyping
 
